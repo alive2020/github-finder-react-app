328c6ca8574079b577ffbcc34944f3ecd3b790dc
Ice K <alive.k001@gmail.com>
2021-03-17 16:34:00 +0900

    completed a project

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,12 +1,8 @@
-import React, {Fragment} from 'react'
-import spinner from './spinner.gif'
+import React, { Fragment } from 'react'
+import spinner from './spinner.gif';
 
-const Spinner = () => {
-    return (
-        <Fragment>
-            <img src={spinner} alt="Loading..." style={{width: '200px', margin: 'auto', display: 'block'}}/>
-        </Fragment>
-    )
-}
+const Spinner = () => <Fragment>
+      <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
+    </Fragment>
 
 export default Spinner
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,32 +1,106 @@
-import React, { Component } from 'react'
+import React, { Fragment, useEffect, useContext } from 'react';
+import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
+import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login)
-    }
-    render() {
-        const { name,
-            avatar_url,
-            location,
-            bio,
-            blog,
-            login,
-            html_url,
-            followers,
-            following,
-            public_repos,
-            public_gists,
-            hireable
-        }= this.props.user;
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
 
-        const {loading} = this.props;
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
-        return (
-            <div>
-                {name}
-            </div>
-        )
-    }
-}
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-export default User
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user;
+
+  if (loading) return <Spinner />;
+
+  return (
+    <Fragment>
+      <Link to='/' className='btn btn-light'>
+        Back To Search
+      </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className='fas fa-check text-success' />
+      ) : (
+        <i className='fas fa-times-circle text-danger' />
+      )}
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            className='round-img'
+            alt=''
+            style={{ width: '150px' }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            Visit Github Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
+            </li>
+
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
+            </li>
+
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong> {blog}
+                </Fragment>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='card text-center'>
+        <div className='badge badge-success'>Followers: {followers}</div>
+        <div className='badge badge-light'>Following: {following}</div>
+        <div className='badge badge-success'>Public Repos: {public_repos}</div>
+        <div className='badge badge-light'>Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
+
+export default User;
